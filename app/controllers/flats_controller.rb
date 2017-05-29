2170392c1e5f1a92a7b8ba7a419040f432ea2a01
Vincent Panis <vincent@come2link.com>
2017-05-30 00:13:42 +0200

    dossiers

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,15 @@
 class FlatsController < ApplicationController
+  before_action :set_flat, only: [:dossiers]
+
+  def dossiers
+    @bookings_pending = Booking.where(status: "pending", flat_id: params[:flat_id])
+    @bookings_confirmed = Booking.where(status: "confirmed", flat_id: params[:flat_id])
+    @bookings_cancelled = Booking.where(status: "cancelled", flat_id: params[:flat_id])
+  end
+
+  private
+
+  def set_flat
+    @flat = Flat.find(params[:flat_id])
+  end
 end
