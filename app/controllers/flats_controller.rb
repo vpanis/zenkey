8dd5d2084844309d59edd0c199502d72a6d13926
alban porcheron <alban.porcheron@gmail.com>
2017-05-29 19:46:39 +0200

    New and edit forms created

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,22 @@
 class FlatsController < ApplicationController
+  before_action :set_flat, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.user = current_user
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:title, :address)
+  end
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
 end
