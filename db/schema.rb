283ae454e0ebbacd0dfb14ce5d2b540c4691351b
Vincent Panis <vincent@come2link.com>
2017-05-29 15:49:15 +0200

    migrate ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170528135702) do
+ActiveRecord::Schema.define(version: 20170529131423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,17 @@ ActiveRecord::Schema.define(version: 20170528135702) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "gender"
+    t.string   "status"
+    t.string   "job_description"
+    t.string   "description"
+    t.integer  "income"
+    t.boolean  "has_warrantor"
+    t.integer  "warrantor_income"
+    t.string   "grade"
+    t.boolean  "is_landlord"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
