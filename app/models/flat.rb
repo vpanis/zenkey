a8d7e9b74a0866ce264e271a98f286064fde44d3
Charles Benard <charles1benard@gmail.com>
2017-05-29 17:10:50 +0200

    add validation to models

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,3 +1,9 @@
 class Flat < ApplicationRecord
   belongs_to :user
+  has_many :availabilities, dependent: :destroy
+  has_many :bookings, dependent: :destroy
+  validates :title, :description, :rental_type, :availability_date, :min_duration, :address, :is_address_public, :subway, :furnished, :size, :rent, :rental_costs, :floor, :rooms, :bedrooms, :balcony, :elevator, :seperate_bathroom, :parking, :user_id, :income_ratio, :has_warrantor, :warrantor_income_ratio, :deposit, presence: true
+  validates :status, presence: true, inclusion: { in: ["libre","occupé"] }
+
+  alias_attribute :landlord_id, :user_id
 end
--- app/models/slot.rb
+++ app/models/slot.rb
@@ -1,4 +1,5 @@
 class Slot < ApplicationRecord
   belongs_to :availability
   belongs_to :user
+  validates :availability_id, :tenant_id, :starts_at, :status, presence: true
 end
