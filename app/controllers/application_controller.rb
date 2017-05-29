c25686388756839ed3cd7a6073254ed280b08845
Charles Benard <charles1benard@gmail.com>
2017-05-29 18:50:22 +0200

    add pundit gem + setup (1)(2) + AddAdminToUsers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,39 @@ class ApplicationController < ActionController::Base
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
+
+    # Pundit: white-list approach.
+    after_action :verify_authorized, except: :index, unless: :skip_pundit?
+    after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+
+    # Uncomment when you *really understand* Pundit!
+    # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+    # def user_not_authorized
+    #   flash[:alert] = "You are not authorized to perform this action."
+    #   redirect_to(root_path)
+    # end
+
+    private
+
+    def skip_pundit?
+      devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+    end
+  end
+  USAGE
+
+  PROTECT RESTAURANT
+
+  rails g pundit:policy restaurant
+  # => generates the file `app/policies/restaurant_policy.rb`
+  Question: Who can create a restaurant?
+  ADMIN USERS
+
+  rails g migration AddAdminToUsers admin:boolean
+  rails db:migrate
+  rails c
+  pry> user = User.where(email: 'seb@lewagon.org').first
+  pry> user.admin = true
+  pry> user.save
+  You can now use this admin field in your policies!
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170529143453) do
+ActiveRecord::Schema.define(version: 20170529164610) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,7 +61,7 @@ ActiveRecord::Schema.define(version: 20170529143453) do
     t.integer  "income_ratio"
     t.boolean  "has_warrantor"
     t.integer  "warrantor_income_ratio"
-    t.integer  "desposit"
+    t.integer  "deposit"
     t.integer  "user_id"
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
@@ -103,6 +103,7 @@ ActiveRecord::Schema.define(version: 20170529143453) do
     t.integer  "warrantor_income"
     t.string   "grade"
     t.boolean  "is_landlord"
+    t.boolean  "admin"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
