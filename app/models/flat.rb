660179cd49fa7f9a65bb18c644d6d37b4cd7d3fa
alban porcheron <alban.porcheron@gmail.com>
2017-05-30 12:53:40 +0200

    New form added

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -5,9 +5,10 @@ class Flat < ApplicationRecord
 
   has_attachments :photos, maximum: 10
 
-  validates :title, :description,  :availability_date, :min_duration, :address, :is_address_public, :subway, :furnished, :size, :rent, :rental_costs, :floor, :rooms, :bedrooms, :balcony, :elevator, :separate_bathroom, :parking, :user_id, :income_ratio, :has_warrantor, :warrantor_income_ratio, :deposit, presence: true
+  validates :title, :description,  :availability_date, :min_duration, :address, :subway, :furnished, :size, :rent, :rental_costs, :floor, :rooms, :bedrooms, :user_id, :income_ratio, :has_warrantor, :warrantor_income_ratio, :deposit, presence: true
   validates :status, presence: true, inclusion: { in: ["libre","occupé"] }
-  validates :rental_type, presence: true, inclusion: { in: ["collaction","location"] }
+  validates :rental_type, presence: true, inclusion: { in: ["Location", "Colocation"] }
+  validates_inclusion_of :is_address_public, :balcony, :elevator, :separate_bathroom, :parking, :in => [true, false]
 
   alias_attribute :landlord_id, :user_id
 end
