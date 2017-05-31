6841c1cee50fca1a9f5f7124933358a29c92a6bc
Vincent Panis <vincent@come2link.com>
2017-05-31 15:32:10 +0200

    small fix

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -12,11 +12,7 @@ module UsersHelper
   end
 
   def set_grade(user, flat)
-    (user.income / flat.rent)
-  end
-
-  def set_grade(user, flat)
-    grade = (user.income + (user.warrantor_income * 0,5)) / (flat.rent + flat.rental.costs)
-    grade = (grade * 2).round / 2
+    grade = (user.income + (user.warrantor_income * 0.5)) / (flat.rent + flat.rental.costs)
+    grade = ((grade * 2).round) / 2
   end
 end
