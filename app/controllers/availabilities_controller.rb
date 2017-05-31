1fd006600bee5bf4248f895f4d4bf05f71d75ea4
alban porcheron <alban.porcheron@gmail.com>
2017-05-31 10:56:34 +0200

    Visits with previous DB

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -3,13 +3,15 @@ class AvailabilitiesController < ApplicationController
   def create
     @availability = Availability.new(availability_params)
     @availability.flat_id = params[:flat_id]
-    @availability.save
-    # Diviser la durée du slot par la durée des visites
-    nb_slots = (@availability.length * 60 ) % @availability.slot_length
-    # Créer n visites
-    nb_slots.times do |i|
-      slot_starting_time = @availability.starts_at + ( i * @availability.slot_length )
-      slot = Slot.create(availability: @availability, starts_at: slot_starting_time , status: "Vacant")
+    if @availability.length * 60 >= @availability.slot_length
+      @availability.save
+      # Diviser la durée du slot par la durée des visites
+      nb_slots = (@availability.length * 60 ) / @availability.slot_length
+      # Créer n visites
+      nb_slots.times do |i|
+        slot_starting_time = @availability.starts_at + ( i * @availability.slot_length * 60 )
+        slot = Slot.create(availability: @availability, starts_at: slot_starting_time , status: "Vacant")
+      end
     end
     authorize @availability
     redirect_to :back
