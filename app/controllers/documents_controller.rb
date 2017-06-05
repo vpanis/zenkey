142526d1b240aef6b9a65a6633a18bd3c883f849
alban porcheron <alban.porcheron@gmail.com>
2017-06-05 09:02:07 +0200

    Started etat des lieux form

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -5,6 +5,15 @@ class DocumentsController < ApplicationController
     authorize (@booking)
   end
 
+  def index
+    @booking = Booking.find(params[:booking_id])
+    @rooms = []
+    for i in 1..@booking.flat.rooms
+      @rooms << "Pièce #{i}"
+    end
+    @documents = policy_scope(Document.all)
+  end
+
   def create
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
     resources :bookings, only: [] do
       patch "booking_confirm" => "bookings#booking_confirm"
       patch "booking_cancel" => "bookings#booking_cancel"
-      resources :documents, only: [:show, :create, :update]
+      resources :documents, only: [:show, :create, :update, :index]
     end
 
     get "visits" => "flats#visits"
